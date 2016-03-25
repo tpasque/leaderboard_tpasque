62e6a045247571ed927c95d2e8e8c5f5acb4a36b
Thomas Pasque <tpasque@gmail.com>
2016-03-25 11:44:58 -0600

    some design changes, header and nav bar.  Bringing in data we want to display, need to paginate results and start working on animations next

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,7 +1,7 @@
 
 app.controller('MainController', ['$scope', 'Board', function($scope, Board){
       Board.results().then(function (results) {
-        $scope.results = results.data;
+        $scope.results = results.data.results
         console.log($scope.results);
       })
     }]);
--- js/services/main_service.js
+++ js/services/main_service.js
@@ -1,6 +1,4 @@
 
-
-
 app.service('Board', ['$http', function($http){
   this.results = function () {
     return $http.get('https://apis.trainheroic.com/public/leaderboard/468425');
